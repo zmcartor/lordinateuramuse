5f84fef00ce4633d336c5e24d74fe559d950c7e1
Zach <zmcartor@gmail.com>
2011-10-28 00:39:54 -0400

    shared imgs pushed to persistent storage

--- app.rb
+++ app.rb
@@ -3,78 +3,111 @@ require 'haml'
 require 'URI'
 require 'mechanize'
 require 'logger'
+require 'mongo_mapper'
+require 'digest/sha1'
+
+
+#custom class for mongo document
+require './post'
+MongoMapper.connection = Mongo::Connection.new('staff.mongohq.com',10058, :pool_size => 5, :timeout => 5)
+MongoMapper.database = 'scrape'
+MongoMapper.database.authenticate('zm','Sup3rFun!')
 
 configure do
-  set :views, "#{File.dirname(__FILE__)}/views"
+	set :views, "#{File.dirname(__FILE__)}/views"
 
-  enable :logging
+	enable :logging
 
-  #setup some app variables here for fun..
-  App_settings = OpenStruct.new(
-	  :title=>'ScuzzleScrape',
-	  :author=>'@zmcartor'
-  )
+	#setup some app variables here for fun..
+	App_settings = OpenStruct.new(
+		:title=>'ScuzzleScrape',
+		:author=>'@zmcartor'
+	)
 
-  Log = Logger.new("sinatra.log")
-  Log.level  = Logger::INFO
+	Log = Logger.new("sinatra.log")
+	Log.level  = Logger::INFO
 end
 
 
 error do
-  e = request.env['sinatra.error']
-  Kernel.puts e.backtrace.join("\n")
-  'Application error'
+	e = request.env['sinatra.error']
+	Kernel.puts e.backtrace.join("\n")
+	'Application error'
 end
 
+set :port , 8080
 
 # root page
-get '/' do
-  haml :index
+get '/' do 
+
+	#show a list of days with content.
+	#show a page of all-time greats
+
+	haml :index
 end
+
 
 get '/scraped' do
 	#because passing in URI, must decode
 	@url= URI::decode params[:url]
-	
+
 	unless @url.start_with? 'http://'
 		@url.insert(0,'http://')
 	end
 
 	#grab with mechanize
-	#likely need some error checking around here.
+	#likely need some better error checking around here.
 
 	Log.info("Going to "+@url)
 	agent = Mechanize.new
-
-	@images, @links = Array.new
-	@body = ''
 
 	#catch any errors that may occur, could be more specific
 	#in handling
 
 	begin
 		agent.get @url do |page|
-			@body = page.body
-			@links = page.image_urls
+
+			@title = page.title
+			@links = page.links
 			@images = page.image_urls
+
 		end
 	rescue
-		Log.info("Not a valid URL, could not read source: "+@url)
-
+		Log.info("Not a valid URL, could not read source or something else bad happened: "+@url)
+		halt haml(:broken)
 	end
-	#grab the url in question
 
-	#check if starts with valid protocol
-
-	# run through and print out resources included on page
-
-	#puts page.body, w00t!!
+	#TODO other feature ideas
+	#for the whole scraped HTML: use page.body
+	#Ensure scraper isn't used as XSS/SQl injection bot
+	haml :scraped
 end
-#grab all images
-#grab all scripts
-#check for security bot thing
-#search around for a 
 
-haml :scraped
+post "/recommend" do 
+	today = Date.today.strftime "%m-%d-%Y"
+	hash = Digest::SHA1.hexdigest params[:url]
+	puts params[:url]
+
+	#TODO no duplicate posts
+	new_post = Post.new(:url=>params[:url] , :date_posted=>today, :info_hash=>hash, :votes=>0)
+	new_post.save	
+end
+
+
+get "/recommended"  do
+	type = params[:type]
+#to get stuff, do
+=begin
+p = Post.new
+p.field = blah
+p.save
+
+to find
+alls = Post.all :conditions =>{:url => something}
+=end
+
+	#1 = imgs
+	#2 = links
+	#3 = kitchen sink
 
 end
--- public/scripts/scrape.js
+++ public/scripts/scrape.js
@@ -1,11 +1,29 @@
 $(function(){
 	$('.scuzz_img_block').mouseenter(function(){
+
+		//if link has been shared, data element will say 'shared'
+		if($(this).data("shared") !="yes"){
 		$(this).append($("<input type='button' value='share' class='share_button'>"))
-		console.log('hoohh')
+		$(".share_button").click(function(){
+		button = $(this)
+		$.ajax({
+			url: "/recommend",
+			type: "POST",
+			data: {url:$(this).siblings('a').attr('href')},
+			success: function(){
+				console.log('submitted')	
+			}
+		})
 	})
+}// if shared check
+
+	});
+
 
 	$('.scuzz_img_block').mouseleave(function(){
 		$(".share_button").remove()
 
 	})
-})
+	
+
+});
