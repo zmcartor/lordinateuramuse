e6005feb033ae5e9772ae92af28eb74a79a7091a
Zach <zmcartor@gmail.com>
2011-11-01 17:09:08 -0400

    Rack enablement: rackup config, thin config file, app subclasses Sinatra::base

--- app.rb
+++ app.rb
@@ -1,4 +1,4 @@
-require 'sinatra'
+require 'sinatra/base'
 require 'haml'
 require 'URI'
 require 'mechanize'
@@ -13,82 +13,83 @@ MongoMapper.connection = Mongo::Connection.new('staff.mongohq.com',10058, :pool_
 MongoMapper.database = 'scrape'
 MongoMapper.database.authenticate('zm','Sup3rFun!')
 
-configure do
-	set :views, "#{File.dirname(__FILE__)}/views"
+class Scuzzle < Sinatra::Base
 
-	enable :logging
+	configure do
+		set :views, "#{File.dirname(__FILE__)}/views"
 
-	#setup some app variables here for fun..
-	APP_SETTINGS = OpenStruct.new(
-		:title=>'ScuzzleScrape',
-		:author=>'@zmcartor'
-	)
+		enable :logging
 
-	Log = Logger.new("sinatra.log")
-	Log.level  = Logger::INFO
-end
+		#setup some app variables here for fun..
+		APP_SETTINGS = OpenStruct.new(
+			:title=>'ScuzzleScrape',
+			:author=>'@zmcartor'
+		)
 
-
-error do
-	e = request.env['sinatra.error']
-	Kernel.puts e.backtrace.join("\n")
-	'Application error'
-end
-
-set :port , 8080
-
-get '/' do 
-	haml :index
-end
-
-
-get '/scraped' do
-	#because passing in URI, must decode
-	@url= URI::decode params[:url]
-
-	unless @url.start_with? 'http://'
-		@url.insert(0,'http://')
+		Log = Logger.new("sinatra.log")
+		Log.level  = Logger::INFO
 	end
 
-	Log.info("Going to "+@url)
-	agent = Mechanize.new
 
-	begin
-		agent.get @url do |page|
+	error do
+		e = request.env['sinatra.error']
+		Kernel.puts e.backtrace.join("\n")
+		'Application error'
+	end
 
-			@title = page.title
-			@links = page.links
-			@images = page.image_urls
+	get '/' do 
+		haml :index
+	end
 
+
+	get '/scraped' do
+		#because passing in URI, must decode
+		@url= URI::decode params[:url]
+
+		unless @url.start_with? 'http://'
+			@url.insert(0,'http://')
 		end
-	rescue
-		Log.info("Not a valid URL, could not read source or something else bad happened: "+@url)
-		halt haml(:broken)
+
+		Log.info("Going to "+@url)
+		agent = Mechanize.new
+
+		begin
+			agent.get @url do |page|
+
+				@title = page.title
+				@links = page.links
+				@images = page.image_urls
+
+			end
+		rescue
+			Log.info("Not a valid URL, could not read source or something else bad happened: "+@url)
+			halt haml(:broken)
+		end
+
+		haml :scraped
 	end
 
-	haml :scraped
-end
+	post "/recommend" do
+		today = Date.today.strftime "%m-%d-%Y"
+		hash = Digest::SHA1.hexdigest params[:url]
 
-post "/recommend" do
-	today = Date.today.strftime "%m-%d-%Y"
-	hash = Digest::SHA1.hexdigest params[:url]
+		new_post = Post.new(:url=>params[:url] , :date_posted=>today, :info_hash=>hash)
+		new_post.save
+	end
 
-	new_post = Post.new(:url=>params[:url] , :date_posted=>today, :info_hash=>hash)
-	new_post.save
-end
+	get "/recommended"  do
+		date = params[:date] || Date.today.strftime("%m-%d-%Y")
+		@posts = Post.all(:created_at => {'$gt' => 1.days.ago.midnight},:order=>'votes DESC')
+		haml :recommended
+	end
 
-get "/recommended"  do
-	date = params[:date] || Date.today.strftime("%m-%d-%Y")
-	@posts = Post.all(:created_at => {'$gt' => 1.days.ago.midnight},:order=>'votes DESC')
-	haml :recommended
-end
+	post "/upvote" do
+		Post.increment({:url=>params[:url]}, :votes => 1)
+		puts 'OK'
+	end
 
-post "/upvote" do
-	Post.increment({:url=>params[:url]}, :votes => 1)
-	puts 'OK'
-end
-
-post "/downvote" do
-	Post.decrement({:url=>params[:url]}, :votes => 1)
-	puts 'OK'
+	post "/downvote" do
+		Post.decrement({:url=>params[:url]}, :votes => 1)
+		puts 'OK'
+	end
 end
