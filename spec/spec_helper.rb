58d6231806578033265fdc11bc49b6c5cedf7161
Zach <zmcartor@gmail.com>
2011-11-02 10:37:08 -0400

    making relative requires consistent

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,16 +1,19 @@
 require 'rack/test'
 require 'webrat'
+require File.join(File.dirname(__FILE__), '..', 'app.rb')
 
 begin 
-	require_relative '../app.rb'
+	require File.join(File.dirname(__FILE__), '..', 'app.rb')
+
 rescue NameError
 	require File.expand_path('../app.rb', __FILE__)
 end
 
 module RSpecMixin
 	include Rack::Test::Methods
-	include Webrat::HaveTagMatcher
-	def app() Sinatra::Application end
+	def app() 
+		Scuzzle 
+	end
 end
 
 Webrat.configure do |config|
@@ -18,7 +21,6 @@ Webrat.configure do |config|
 end
 
 RSpec.configure  do |config|
-	config.include Webrat::HaveTagMatcher
 	config.include Webrat
 	config.include RSpecMixin
 	config.include Haml::Helpers  
